389b3a9f168ce4b0932ee8c5314bd8b51161ebd4
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2025-05-03 14:09:58 +0300

    made it work with different lang

--- src/collate.py
+++ src/collate.py
@@ -5,8 +5,8 @@ def collate(elems: tuple) -> tuple:
     # Determine if CUDA is available (for GPU) or fallback to CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    words, labels = list(zip(*elems))
+    words, labels, langs = list(zip(*elems))
     pad_labels = pad_sequence(labels, batch_first=True, padding_value=0)
- 
+
     # Berke changed this code, it was: return list(words), pad_labels.cuda()
-    return list(words), pad_labels.to(device)
+    return list(words), pad_labels.to(device), langs
