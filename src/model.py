3cbf216a361a5d9b014daff49ef7830979c97f25
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2025-05-09 18:02:27 +0300

    model fixed i think

--- src/model.py
+++ src/model.py
@@ -7,97 +7,89 @@ import torch.nn.functional as F
 class IdiomExtractor(nn.Module):
     def __init__(self,
                  bert_model,
-                 bert_config,
-                 hparams,
-                 device,
-                 useLSTM=False):
+                 hparams):
         super(IdiomExtractor, self).__init__()
         pprint(hparams)
 
-        self.bert_model = bert_model
-        self.bert_config = bert_config
-        self.hparams = hparams
-        self.device = device
-        self.useLSTM = useLSTM
+        self.bert = bert_model
+        self.use_lstm = hparams.use_lstm
+        self.hidden_size = bert_model.config.hidden_size
 
         # lstm and bert output dimension will be the same
-
-        # batch normalization layers 
-        self.bn_emb = nn.BatchNorm1d(self.bert_config.hidden_size) # hidden size of 768
-        self.bn_lstm = nn.BatchNorm1d(self.bert_config.hidden_size)
-
-        # dropout layer with 0.5 dropout rate
-        self.dropout = nn.Dropout(hparams.dropout)
-
-        # use bidirectional lstm
-        self.lstm = nn.LSTM(self.bert_config.hidden_size,
-                            self.bert_config.hidden_size // 2 if hparams.bidirectional else self.bert_config.hidden_size,
-                            bidirectional=self.hparams.bidirectional, 
-                            num_layers=self.hparams.num_layers,
-                            dropout=self.hparams.dropout if self.hparams.num_layers>1 else 0,
-                            batch_first=True)
-        
+        if self.use_lstm:
+            self.lstm = nn.LSTM(
+                self.hidden_size, # input size = hidden size of bert -> 768
+                # output size = hidden size of lstm -> 768
+                self.hidden_size // 2 if hparams.bidirectional else self.hidden_size,
+                batch_first=True, # shape = (batch, seq_len, hidden_size)
+                bidirectional=hparams.bidirectional,
+                num_layers=hparams.num_layers,
+                dropout=hparams.dropout if hparams.num_layers > 1 else 0,
+            )
 
         self.classifier = nn.Sequential(
-            nn.Linear(self.bert_config.hidden_size, 256),
+            nn.Linear(self.hidden_size, 256),
             nn.LayerNorm(256),
             nn.ReLU(),
             nn.Dropout(hparams.dropout),
             nn.Linear(256, 128),
+            nn.LayerNorm(128),
             nn.ReLU(),
             nn.Dropout(hparams.dropout),
             nn.Linear(128, hparams.num_classes),
         )
 
+        # dropout layer with 0.5 dropout rate
+        self.dropout = nn.Dropout(hparams.dropout)
+
         # we use a CRF layer to model the dependencies between the labels
         self.CRF = CRF(hparams.num_classes, batch_first=True).cuda()
  
     def freeze_bert(self):
-        for param in self.bert_model.parameters():
+        for param in self.bert.parameters():
             param.requires_grad = False
         print("BERT model parameters have been frozen.")
     
     def unfreeze_bert(self):
-        for param in self.bert_model.parameters():
+        for param in self.bert.parameters():
             param.requires_grad = True
         print("BERT model parameters have been unfrozen.")
     
     def forward(self, bert_embeddings, labels):
 
-        # paddlenmiş kısımlara attande etmemek için mask oluşturuyoruz
-        mask = self.padding_mask(labels)
+        # trainde label olacak ve pad edilmiş kısımlara attend etmemek için mask oluşturuyoruz
+        if labels is not None:
+            # padding olan kısımlara attande etmemek için mask oluşturuyoruz
+            mask = labels.ne(0)
+        # test kısmında label olmayak bu yüzden tüm kısımlara attend etmemiz lazım
+        else:
+            mask = torch.ones(bert_embeddings.shape[:2], dtype=torch.bool,
+                              device=bert_embeddings.device)
+        # CRF ilk time‐step’in unmasked olmasını ister
+        mask[:, 0] = True
         
         # Apply batch norm before dropout (need to reshape for BatchNorm1d)
         batch_size, seq_len, hidden_size = bert_embeddings.shape
-        X = nn.functional.layer_norm(bert_embeddings, normalized_shape=[hidden_size])
-        
-        # bert embeddingse dropout uygulamak saçma geldi
-        #bert_embeddings = self.dropout(bert_embeddings)
 
-        # Apply LSTM to the BERT embeddings
-        if self.useLSTM:
-            X, (h, c) = self.lstm(X)
-            # Apply batch norm to LSTM output
-            batch_size, seq_len, hidden_size = X.shape
+        # apply dropout -> bu saçma değilmiş gerekliymiş
+        # ayrıca bertin kendi içinde layernormu var o yüzen layer norma gerek yok
+        X = self.dropout(bert_embeddings)
+
+
+        if self.use_lstm:
+            X,_ = self.lstm(X)
             X = nn.functional.layer_norm(X, normalized_shape=[hidden_size])
-            # Apply dropout to LSTM output
             X = self.dropout(X)
 
+        
         # Apply linear layer
-        O = self.classifier(X)
+        emissions  = self.classifier(X)
 
+        # eğer label yoksa (inference) decode etmemiz lazım
         if labels is None:
-            log_likelihood = -100
-        else:
-            log_likelihood = self.CRF.forward(O, labels, mask)
+            return self.CRF.decode(emissions, mask=mask)
+        
+        
+        log_likelihood = self.CRF.forward(emissions, labels, mask)
 
-        return log_likelihood, O
-
-
-    def padding_mask(self, labels: torch.Tensor) -> torch.Tensor:
-
-        # create a bool mask: True for real tokens, False for padding
-        mask = labels.ne(0)            # shape: (batch, seq_len), dtype=bool
-        # CRF requires the first timestep unmasked
-        mask[:, 0] = True
-        return mask    
+        return log_likelihood, emissions
