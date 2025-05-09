5de1e2390d5fe8143f132db5d8094894c6b63929
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2025-05-09 16:28:54 +0300

    sentence level language support added

--- src/collate.py
+++ src/collate.py
@@ -8,11 +8,26 @@ def collate(elems: tuple) -> tuple:
     # unzip the batch of triples
     words, label_seqs, lang_seqs = zip(*elems)
 
-    pad_labels = pad_sequence(label_seqs, batch_first=True, padding_value=-1)
+    pad_labels = pad_sequence(label_seqs, batch_first=True, padding_value=0)
     pad_langs  = pad_sequence(lang_seqs,  batch_first=True, padding_value=-1)
 
     # move both to the device
     pad_labels = pad_labels.to(device)
-    pad_langs  = pad_langs.to(device)
 
-    return list(words), pad_labels, pad_langs
+    # cümlelerin pad öncesi uzunluğu
+    seq_lens = [len(w) for w in words]  # örn. [8, 12, 5, …]
+
+    # get sentence level language labels
+    sent_langs = []
+    for i, L in enumerate(seq_lens):
+        if L > 0:
+            # get first language label as sentence level label
+            lang0 = pad_langs[i, 0].item()
+            sent_langs.append(lang0)
+        else:
+            # boş cümle olursa -1 atayalım
+            sent_langs.append(-1)
+
+    sent_langs = torch.tensor(sent_langs, device=device)  # shape == (batch_size,)
+
+    return list(words), pad_labels, sent_langs
