82fec85ad8fa7e7c16c25ff3b2c5034b399d90e8
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2025-05-09 18:02:38 +0300

    additional parameters added

--- src/hparams.py
+++ src/hparams.py
@@ -1,5 +1,8 @@
+from __init__ import *
 class HParams():
     dropout = 0.5
     num_classes = 4
     bidirectional = True
     num_layers = 2
+    use_lstm = True
+    device = "cuda" if torch.cuda.is_available() else "cpu"
