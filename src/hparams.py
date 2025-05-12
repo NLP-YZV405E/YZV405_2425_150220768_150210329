a64b92fcd113a66ec76b4088d8daa4ee55b905fc
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2025-05-12 20:45:14 +0300

    new hparams

--- src/hparams.py
+++ src/hparams.py
@@ -4,14 +4,14 @@ class HParams():
     dropout = 0.3
     num_classes = 4
     bidirectional = True
-    num_layers = 3
+    num_layers = 2
     use_lstm = True
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
     # Training Parameters
     batch_size = 32
     lr = 0.0005
-    epoch = 100
+    epoch = 50
     
     # New Parameters
     warmup_steps = 1000
